0384a9d2c835a98e6451412f2016f6f3e8f9391c
jcass77 <john.cass77@gmail.com>
2020-12-30 12:18:55 +0200

    Refactor.

--- 16/gendates.py
+++ 16/gendates.py
@@ -4,14 +4,8 @@ PYBITES_BORN = datetime(year=2016, month=12, day=19)
 
 
 def gen_special_pybites_dates():
-    i = 1
-    next_birthday = PYBITES_BORN + timedelta(days=365)
-
+    days = 0
     while True:
-        next_100_days = PYBITES_BORN + timedelta(days=i * 100)
-        if next_100_days > next_birthday:
-            yield next_birthday
-            next_birthday = next_birthday + timedelta(days=365)
-        else:
-            yield next_100_days
-            i += 1
+        days += 1
+        if days % 100 == 0 or days % 365 == 0:
+            yield PYBITES_BORN + timedelta(days=days)
