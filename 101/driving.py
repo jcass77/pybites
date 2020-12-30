64b603d665c2707f7ec336da8ba9b4650fd9cd78
jcass77 <john.cass77@gmail.com>
2020-12-30 07:17:17 +0200

    Refactor.

--- 101/driving.py
+++ 101/driving.py
@@ -4,4 +4,5 @@ MIN_DRIVING_AGE = 18
 def allowed_driving(name, age):
     """Print '{name} is allowed to drive' or '{name} is not allowed to drive'
        checking the passed in age against the MIN_DRIVING_AGE constant"""
-    print(f"{name} is{' not ' if age < MIN_DRIVING_AGE else ' '}allowed to drive")
+    is_allowed = 'is allowed' if age >= MIN_DRIVING_AGE else 'is not allowed'
+    print(f'{name} {is_allowed} to drive')
