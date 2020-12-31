4576f92930406b6f6af846c3edfe3d7a9e441a4d
jcass77 <john.cass77@gmail.com>
2020-12-31 09:40:38 +0200

    Refactor.

--- 68/clean.py
+++ 68/clean.py
@@ -1,6 +1,7 @@
 import re
+import string
 
 
 def remove_punctuation(input_string):
     """Return a str with punctuation chars stripped out"""
-    return re.sub("[!\"#$%&\\'()*+,-./:;<=>?@[\\]^_`{|}~]", "", input_string)
+    return re.sub(f"[{re.escape(string.punctuation)}]", "", input_string)
