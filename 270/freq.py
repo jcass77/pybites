dc37059f6118cd716523e40beb64b7409f0b3268
jcass77 <john.cass77@gmail.com>
2021-01-02 17:23:03 +0200

    Refactor.

--- 270/freq.py
+++ 270/freq.py
@@ -1,9 +1,5 @@
 import collections
-from operator import itemgetter
 
 
 def freq_digit(num: int) -> int:
-    counts = sorted(
-        collections.Counter(str(num)).items(), key=itemgetter(1), reverse=True
-    )
-    return int(counts[0][0])
+    return int(collections.Counter(str(num)).most_common()[0][0])
