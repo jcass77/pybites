02fb76c1a02e6997a581110c6023e3bc284cbdb0
jcass77 <john.cass77@gmail.com>
2021-01-04 14:13:54 +0200

    Refactor.

--- 29/wrong_char.py
+++ 29/wrong_char.py
@@ -1,11 +1,8 @@
 import collections
-import string
-
-ALPHA_NUMERIC = string.ascii_letters + string.digits
 
 
 def get_index_different_char(chars: list):
-    is_alpha = [c and str(c) in ALPHA_NUMERIC for c in chars]
-    most_common = collections.Counter(is_alpha).most_common(1)[0][0]
+    is_alnum = [str(c).isalnum() for c in chars]
+    most_common = collections.Counter(is_alnum).most_common(1)[0][0]
 
-    return is_alpha.index(not most_common)
+    return is_alnum.index(not most_common)
