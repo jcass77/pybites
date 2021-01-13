2e4b7f3b4f4634251a8253ccc96edf89fdd05ab3
jcass77 <john.cass77@gmail.com>
2021-01-13 16:54:29 +0200

    Refactor.

--- 17/friends.py
+++ 17/friends.py
@@ -5,4 +5,5 @@ from itertools import combinations, permutations
 def friends_teams(
     friends: List[str], team_size: int = 2, order_does_matter: bool = False
 ):
-    return permutations(friends, r=team_size) if order_does_matter else combinations(friends, r=team_size)
+    func = permutations if order_does_matter else combinations
+    return func(friends, r=team_size)
