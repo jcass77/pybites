929896ce73473fa9726904802c0870c51ea7bc30
jcass77 <john.cass77@gmail.com>
2021-01-13 16:19:45 +0200

    Refactor.

--- 66/running_mean.py
+++ 66/running_mean.py
@@ -1,6 +1,10 @@
+import itertools
+
+
 def running_mean(sequence):
     """Calculate the running mean of the sequence passed in,
     returns a sequence of same length with the averages.
     You can assume all items in sequence are numeric."""
 
-    return (round(sum(sequence[0:i]) / i, 2) for i in range(1, len(sequence) + 1))
+    for i, num in enumerate(itertools.accumulate(sequence), start=1):
+        yield round(num / i, 2)
