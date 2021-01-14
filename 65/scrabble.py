45a61fa07bf3b0674a5c664e73194ccfe8718a25
jcass77 <john.cass77@gmail.com>
2021-01-14 12:13:25 +0200

    Refactor.

--- 65/scrabble.py
+++ 65/scrabble.py
@@ -3,9 +3,6 @@ import os
 import urllib.request
 
 # PREWORK
-import bisect
-from typing import List
-
 TMP = os.getenv("TMP", "/tmp")
 DICT = "dictionary.txt"
 DICTIONARY = os.path.join(TMP, DICT)
@@ -14,21 +11,19 @@ urllib.request.urlretrieve(
 )
 
 with open(DICTIONARY) as f:
-    dictionary = sorted(set([word.strip().lower() for word in f.read().split()]))
-
-
-def is_word(perm: str, sorted_dictionary: List[str]):
-    i = bisect.bisect_left(sorted_dictionary, perm)
-    if i != len(dictionary) and sorted_dictionary[i] == perm:
-        return True
-    return False
+    dictionary = set([word.strip().lower() for word in f.read().split()])
 
 
 def get_possible_dict_words(draw):
-    return (perm for perm in _get_permutations_draw(draw) if is_word(perm, dictionary))
+    """Get all possible words from a draw (list of letters) which are
+    valid dictionary words. Use _get_permutations_draw and provided
+    dictionary"""
+    permutations = ("".join(word).lower() for word in _get_permutations_draw(draw))
+    return set(permutations) & dictionary
 
 
 def _get_permutations_draw(draw):
-    for i in range(len(draw)):
-        for perm in itertools.permutations(draw, r=i):
-            yield "".join(perm).lower()
+    """Helper to get all permutations of a draw (list of letters), hint:
+    use itertools.permutations (order of letters matters)"""
+    for i in range(1, len(draw) + 1):
+        yield from itertools.permutations(draw, i)
