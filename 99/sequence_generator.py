ac6e85282149bce0968fbde4bbb0927409e437de
johncass <john.cass77@gmail.com>
2021-01-16 08:05:17 +0200

    submission Bite 99 @ codechalleng.es

--- 99/sequence_generator.py
+++ 99/sequence_generator.py
@@ -1,2 +1,7 @@
+import itertools
+import string
+
+
 def sequence_generator():
-    pass
+    seq = ((d, c) for d, c in enumerate(string.ascii_uppercase, start=1))
+    return itertools.cycle(itertools.chain.from_iterable(seq))
