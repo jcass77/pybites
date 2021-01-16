b80041ecce6cb88f210b0eaaa0638513faab94ce
johncass <john.cass77@gmail.com>
2021-01-16 08:07:42 +0200

    Refactor.

--- 99/sequence_generator.py
+++ 99/sequence_generator.py
@@ -1,7 +1,7 @@
-import itertools
-import string
+from itertools import chain, cycle
+from string import ascii_uppercase
 
 
 def sequence_generator():
-    seq = ((d, c) for d, c in enumerate(string.ascii_uppercase, start=1))
-    return itertools.cycle(itertools.chain.from_iterable(seq))
+    seq = ((d, c) for d, c in enumerate(ascii_uppercase, start=1))
+    return cycle(chain.from_iterable(seq))
