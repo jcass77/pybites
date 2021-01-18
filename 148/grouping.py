18e2878e16e58525e579f5395e71aacae15f8e26
johncass <john.cass77@gmail.com>
2021-01-18 09:53:00 +0200

    submission Bite 148 @ codechalleng.es

---
+++ 148/grouping.py
@@ -0,0 +1,47 @@
+import itertools
+from operator import itemgetter
+
+cars = [
+    # need mock data? -> https://www.mockaroo.com == awesome
+    ("Mercedes-Benz", "300D"),
+    ("Mercedes-Benz", "600SEL"),
+    ("Toyota", "Avalon"),
+    ("Ford", "Bronco"),
+    ("Chevrolet", "Cavalier"),
+    ("Chevrolet", "Corvette"),
+    ("Mercedes-Benz", "E-Class"),
+    ("Hyundai", "Elantra"),
+    ("Volkswagen", "GTI"),
+    ("Toyota", "Highlander"),
+    ("Chevrolet", "Impala"),
+    ("Nissan", "Maxima"),
+    ("Ford", "Mustang"),
+    ("Kia", "Optima"),
+    ("Volkswagen", "Passat"),
+    ("Nissan", "Pathfinder"),
+    ("Volkswagen", "Routan"),
+    ("Hyundai", "Sonata"),
+    ("Kia", "Sorento"),
+    ("Kia", "Sportage"),
+    ("Ford", "Taurus"),
+    ("Nissan", "Titan"),
+    ("Toyota", "Tundra"),
+    ("Hyundai", "Veracruz"),
+]
+
+
+def group_cars_by_manufacturer(cars):
+    """Iterate though the list of (manufacturer, model) tuples
+    of the cars list defined above and generate the output as described
+    in the Bite description (see the tests for the full output).
+
+    No return here, just print to the console. We use pytest > capfd to
+    validate your output :)
+    """
+    for manufacturer, models in itertools.groupby(
+        sorted(cars, key=itemgetter(0)), key=itemgetter(0)
+    ):
+        print(f"{manufacturer.upper()}")
+        for model in models:
+            print(f"- {model[1]}")
+        print("")
