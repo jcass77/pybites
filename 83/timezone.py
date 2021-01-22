550872357e78af2c6b68d79d71707467f15cb0b3
johncass <john.cass77@gmail.com>
2021-01-22 15:04:07 +0200

    Refactor.

--- 83/timezone.py
+++ 83/timezone.py
@@ -9,5 +9,5 @@ SPAIN = timezone("Europe/Madrid")
 def what_time_lives_pybites(naive_utc_dt: datetime):
     """Receives a naive UTC datetime object and returns a two element
     tuple of Australian and Spanish (timezone aware) datetimes"""
-    aware_dt = naive_utc_dt.replace(tzinfo=utc)
+    aware_dt = utc.localize(naive_utc_dt)
     return aware_dt.astimezone(AUSTRALIA), aware_dt.astimezone(SPAIN)
