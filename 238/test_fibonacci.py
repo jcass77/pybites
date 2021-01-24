77b49c56d6ccf472145e15a34f01f0e1eef139b0
johncass <john.cass77@gmail.com>
2021-01-24 06:49:46 +0200

    Refactor.

--- 238/test_fibonacci.py
+++ 238/test_fibonacci.py
@@ -1,8 +1,9 @@
-import random
-
 import pytest
 
 from fibonacci import fib
+
+
+EXPECTED = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
 
 
 def test_fib_negative_value_raises_error():
@@ -11,9 +12,15 @@ def test_fib_negative_value_raises_error():
 
 
 def test_fib_less_than_two_returns_input_parameter_as_is():
-    assert all(fib(i) == i for i in [0, 1])
+    assert fib(0) == 0
+    assert fib(1) == 1
 
 
-@pytest.mark.parametrize("n", (random.randint(2, 20) for _ in range(5)))
-def test_fib_calculates_correct_sequence(n):
-    assert fib(n) == fib(n-1) + fib(n-2)
+@pytest.mark.parametrize("x, y", list(enumerate(EXPECTED)))
+def test_fib_calculates_correct_sequence(x, y):
+    assert fib(x) == y
+
+
+# Run at least one test with n >= 10 to keep MutPy happy :(
+def test_fib_calculates_correct_sequence_10():
+    assert fib(10) == 55
