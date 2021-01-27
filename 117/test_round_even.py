c0a023cbe680cd8d14d0193300c890788e1b8d8c
johncass <john.cass77@gmail.com>
2021-01-27 18:40:47 +0200

    submission Bite 117 @ codechalleng.es

--- 117/test_round_even.py
+++ 117/test_round_even.py
@@ -3,17 +3,14 @@ import pytest
 from round_even import round_even
 
 
-@pytest.mark.parametrize(
-    "arg, expected",
-    [
-        (0.45, 0.4),  # nearest even 10e-1
-        (0.55, 0.6),
-        (0.65, 0.6),  # nearest even 10e-1
-        (1.45, 1.4),  # nearest even 10e-1
-        (1.55, 1.6),
-        (1.65, 1.6),  # nearest even 10e-1
-        (2.55, 2.6),  # nearest even 10e-1
-    ],
-)
+@pytest.mark.parametrize("arg, expected", [
+    (0.4, 0),
+    (0.5, 0),  # nearest even int
+    (0.6, 1),
+    (1.4, 1),
+    (1.5, 2),
+    (1.6, 2),
+    (2.5, 2),  # nearest even int
+])
 def test_round_even(arg, expected):
     assert round_even(arg) == expected
