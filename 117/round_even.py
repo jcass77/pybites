7d7bd8a8a600330e17070f45844afbae57445054
johncass <john.cass77@gmail.com>
2021-01-28 06:36:39 +0200

    Refactor.

--- 117/round_even.py
+++ 117/round_even.py
@@ -3,7 +3,7 @@ from decimal import Decimal, ROUND_HALF_EVEN
 
 def round_even(number):
     return float(round(Decimal(str(number)), 1))
-    # return round(number, 1)
+    # return round(number)
     # return float(Decimal(number).quantize(Decimal("0.1")))
     # d = Decimal(str(number))
     # return float(d.quantize(Decimal("0.1"), rounding=ROUND_HALF_EVEN))
