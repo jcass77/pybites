d3cf063d0860b244e3f0b218bbcf818d0cc8ba8a
johncass <john.cass77@gmail.com>
2021-01-31 19:54:23 +0200

    Refactor.

--- pybites_bite117/round_even.py
+++ pybites_bite117/round_even.py
@@ -1,8 +1,9 @@
-from decimal import Decimal, ROUND_HALF_EVEN
+from decimal import Decimal, ROUND_HALF_UP
 
 
 def round_even(number):
-    return float(round(Decimal(str(number)), 1))
+    precise_number = Decimal(str(number))
+    return float(precise_number.quantize(Decimal("0.1"), rounding=ROUND_HALF_UP))
     # return round(number)
     # return float(Decimal(number).quantize(Decimal("0.pybites_bite1")))
     # d = Decimal(str(number))
