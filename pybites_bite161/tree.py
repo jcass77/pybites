330f906c1ae65aa08ab1097c37237c0734e4fd52
johncass <john.cass77@gmail.com>
2021-02-08 06:46:31 +0200

    Refactor.

--- pybites_bite161/tree.py
+++ pybites_bite161/tree.py
@@ -1,20 +1,14 @@
-from collections import Counter
-from pathlib import Path
+import os
 
 
 def count_dirs_and_files(directory="."):
     """Count the amount of of directories and files in passed in "directory" arg.
     Return a tuple of (number_of_directories, number_of_files)
     """
-    if type(directory) is str:
-        directory = Path(directory)
+    dirs, files = 0, 0
 
-    count = Counter()
-    for x in directory.iterdir():
-        if x.is_dir():
-            count["dir"] += 1
-            count["file"] += count_dirs_and_files(x)[1]
-        elif x.is_file():
-            count["file"] += 1
+    for _, directories, filenames in os.walk(directory):
+        dirs += len(directories)
+        files += len(filenames)
 
-    return count["dir"], count["file"]
+    return dirs, files
