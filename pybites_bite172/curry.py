1df99855b63ba2ced0b619860970077005e5f041
johncass <john.cass77@gmail.com>
2021-02-08 09:02:40 +0200

    submission Bite 172 @ codechalleng.es

---
+++ pybites_bite172/curry.py
@@ -0,0 +1,7 @@
+from functools import partial
+
+# create 2 partials:
+# - 'rounder_int' rounds to int (0 places)
+# - 'rounder_detailed' rounds to 4 places
+rounder_int = partial(lambda n: round(n, 0))
+rounder_detailed = partial(lambda n: round(n, 4))
