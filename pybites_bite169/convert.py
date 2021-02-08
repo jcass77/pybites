4528a777bc30e76db7665a729db604b5aa72dbed
johncass <john.cass77@gmail.com>
2021-02-08 08:45:46 +0200

    Refactor.

--- pybites_bite169/convert.py
+++ pybites_bite169/convert.py
@@ -1,3 +1,6 @@
+FORMATS = {"cm": 2.54, "in": 0.393_700_787_4}
+
+
 def convert(value: float, fmt: str) -> float:
     """Converts the value to the designated format.
 
@@ -5,14 +8,12 @@ def convert(value: float, fmt: str) -> float:
     :param fmt: String indicating format to convert to
     :return: Float rounded to 4 decimal places after conversion
     """
-    if type(value) not in [float, int]:
-        raise TypeError(f"Expected 'value' to be either a float or an int!")
-
     fmt = fmt.lower()
-    if fmt not in ["cm", "in"]:
+
+    if fmt not in FORMATS.keys():
         raise ValueError(f"Expected 'fmt' to be either 'cm' or 'in'!")
 
-    if fmt == "cm":
-        return round(value * 2.54, 4)
-
-    return round(value * 0.3937008, 4)
+    try:
+        return round(value * FORMATS[fmt], 4)
+    except TypeError as e:
+        raise TypeError(f"Expected '{value}' to be either a float or an int!")
