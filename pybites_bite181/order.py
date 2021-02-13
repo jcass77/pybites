6a3571503ee643704df2fe654dd7741be5594c9b
johncass <john.cass77@gmail.com>
2021-02-13 14:47:23 +0200

    Refactor.

--- pybites_bite181/order.py
+++ pybites_bite181/order.py
@@ -6,12 +6,7 @@ class OrderedList:
         self._numbers = []
 
     def add(self, num):
-        "Find leftmost value greater than x"
-        i = bisect.bisect_right(self._numbers, num)
-        if i != len(self._numbers):
-            self._numbers.insert(i, num)
-        else:
-            self._numbers.append(num)
+        bisect.insort(self._numbers, num)
 
     def __str__(self):
         return ", ".join(str(num) for num in self._numbers)
