d95fe084f0a95c9608a4c5318651125985091fc1
johncass <john.cass77@gmail.com>
2021-02-16 08:34:13 +0200

    submission Bite 209 @ codechalleng.es

---
+++ pybites_bite209/docstring.py
@@ -0,0 +1,11 @@
+def sum_numbers(numbers):
+    """
+    Sums numbers
+
+    :param numbers: a list of numbers
+    :type numbers: list
+    :raises TypeError: if not all numeric values are passed in
+    :return: sum of numbers
+    :rtype: int
+    """
+    pass
