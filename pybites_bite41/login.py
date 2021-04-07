a7505c06f36d60ea159106cd1efe4e03c8549f65
johncass <john.cass77@gmail.com>
2021-04-07 15:36:04 +0200

    submission Bite 41 @ codechalleng.es

---
+++ pybites_bite41/login.py
@@ -0,0 +1,25 @@
+from collections import Callable
+from functools import wraps
+
+known_users = ["bob", "julian", "mike", "carmen", "sue"]
+loggedin_users = ["mike", "sue"]
+
+
+def login_required(func: Callable):
+    @wraps(func)
+    def func_wrapper(user):
+        if user not in known_users:
+            return "please create an account"
+
+        if user not in loggedin_users:
+            return "please login"
+
+        return f"welcome back {user}"
+
+    return func_wrapper
+
+
+@login_required
+def welcome(user):
+    """Return a welcome message if logged in"""
+    pass
