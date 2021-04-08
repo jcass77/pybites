88217ed04fc94d0f022a1eff3fb0c9c320d008e2
johncass <john.cass77@gmail.com>
2021-04-08 07:15:29 +0200

    Refactor.

--- pybites_bite49/packt.py
+++ pybites_bite49/packt.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-from bs4 import BeautifulSoup as Soup
+from bs4 import BeautifulSoup
 import requests
 
 PACKT = "https://bites-data.s3.us-east-2.amazonaws.com/packt.html"
@@ -11,13 +11,15 @@ Book = namedtuple("Book", "title description image link")
 
 def get_book():
     """make a Soup object, parse the relevant html sections, and return a Book namedtuple"""
-    soup = Soup(CONTENT)
+    soup = BeautifulSoup(CONTENT, "html.parser")
 
     book_main = soup.find("div", class_="dotd-main-book")
-    title_div = book_main.find("div", class_="dotd-title")
-    description = title_div.find_next_sibling("div").text.strip()
-    image_div = book_main.find("div", class_="dotd-main-book-image")
-    image = image_div.find("img").get("src")
-    link = image_div.find("a").get("href")
 
-    return Book(title_div.text.strip(), description, image, link)
+    title_div = book_main.find("div", class_="dotd-title")
+    description = title_div.find_next_sibling("div").get_text(strip=True)
+
+    image_div = book_main.find("div", class_="dotd-main-book-image")
+    image = image_div.find("img")["src"]
+    link = image_div.find("a")["href"]
+
+    return Book(title_div.get_text(strip=True), description, image, link)
