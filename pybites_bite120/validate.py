78ac566ff8d9cdd8ddce704f2169db58a4ab4060
johncass <john.cass77@gmail.com>
2021-04-17 05:08:15 +0200

    Refactor.

--- pybites_bite120/validate.py
+++ pybites_bite120/validate.py
@@ -1,11 +1,12 @@
 from functools import wraps
+from numbers import Integral
 
 
 def int_args(func):
     @wraps(func)
     def func_wrapper(*args, **kwargs):
         for arg in args:
-            if type(arg) is not int:
+            if not isinstance(arg, Integral):
                 raise TypeError("Arguments must be integers")
 
             if arg < 0:
